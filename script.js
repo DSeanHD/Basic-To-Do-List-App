820d760e6d760a583826aaf7ed91f4b842aa4ae6
DSeanHD <danielseanfolkes@gmail.com>
2024-07-21 19:28:24 -0500

    Allow items to be stored in local storage

--- script.js
+++ script.js
@@ -16,11 +16,17 @@ document.getElementById('date').innerHTML = "<b>Date:</b> " + dateVar.toLocaleDa
 
 let listItems = document.getElementById("list-items");
 let userInput = document.getElementById("input-items");
+const addItemsBtn = document.getElementById("addItemsBtn");
+
+let listStorage = JSON.parse(localStorage.getItem("todo-items")) || [];
 
 const addItems = () => {
     if (userInput.value === "") {
         return null;
     } else {
+        listStorage.push(userInput.value);
+        localStorage.setItem("todo-items", JSON.stringify(listStorage));
+
         let li = document.createElement("li");
         li.innerText = userInput.value;
         listItems.appendChild(li);
@@ -35,13 +41,23 @@ const addItems = () => {
 
         deleteBtn.addEventListener("click", () => {
             listItems.removeChild(li);
+            // Remove item from localStorage
+            listStorage = listStorage.filter(item => item !== userInput.value);
+            localStorage.setItem("todo-items", JSON.stringify(listStorage));
         });
 
         editBtn.addEventListener("click", () => {
-            let editText = prompt();
+            let editText = prompt("Edit your item:", userInput.value);
             if (editText === null || editText === "") {
                 return null;
             } else {
+                // Update the item in localStorage
+                let index = listStorage.indexOf(userInput.value);
+                if (index !== -1) {
+                    listStorage[index] = editText;
+                    localStorage.setItem("todo-items", JSON.stringify(listStorage));
+                }
+
                 li.innerText = editText;
                 li.appendChild(deleteBtn);
                 li.appendChild(editBtn);
@@ -50,7 +66,48 @@ const addItems = () => {
     }
 }
 
-const addItemsBtn = document.getElementById("addItemsBtn");
+let listParse = JSON.parse(localStorage.getItem("todo-items"));
+
+window.addEventListener("load", () => {
+    listParse.forEach(item => {
+        let li = document.createElement("li");
+        li.innerText = item;
+        listItems.appendChild(li);
+
+        let deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "Delete";
+        li.appendChild(deleteBtn);
+
+        let editBtn = document.createElement("button");
+        editBtn.innerText = "Edit";
+        li.appendChild(editBtn);
+
+        deleteBtn.addEventListener("click", () => {
+            listItems.removeChild(li);
+            // Remove item from localStorage
+            listStorage = listStorage.filter(listItem => listItem !== item);
+            localStorage.setItem("todo-items", JSON.stringify(listStorage));
+        });
+
+        editBtn.addEventListener("click", () => {
+            let editText = prompt("Edit your item:", item);
+            if (editText === null || editText === "") {
+                return null;
+            } else {
+                // Update the item in localStorage
+                let index = listStorage.indexOf(item);
+                if (index !== -1) {
+                    listStorage[index] = editText;
+                    localStorage.setItem("todo-items", JSON.stringify(listStorage));
+                }
+
+                li.innerText = editText;
+                li.appendChild(deleteBtn);
+                li.appendChild(editBtn);
+            }
+        });
+    });
+});
 
 addItemsBtn.addEventListener("click", addItems);
 userInput.addEventListener("keydown", (e) => {
